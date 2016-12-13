7b8f1d6d5b954b7ff09b37654f3e0df660bde4c7
eduard <blackbars48@gmail.com>
2016-12-13 17:54:06 +0200

    some test

--- www/templates/services/localStorageService.js
+++ www/templates/services/localStorageService.js
@@ -5,7 +5,10 @@ angular
   function localStorageService(apiInfoService) {
 
     function checkCurrencyList() {
-    return !!localStorage.currencyList ||  setCurrencyList()
+      return !!localStorage.currencyList ||  setCurrencyList()
+    }
+    function checkCurrencyPairList() {
+      return !!localStorage.currencyPairList || (localStorage.currencyPairList = '[]')
     }
     function setCurrencyList() {
       apiInfoService.getCurrencyList().then(function (resp) {
@@ -15,6 +18,9 @@ angular
         console.log(list);
         localStorage.currencyList = JSON.stringify(list)
       })
+    }
+    var setCurrencyPair = function () {
+
     }
     function getCurrencyList() {
       return JSON.parse(localStorage.currencyList)
